7cff02aea26b40d9d193180f6dd560b7be205570
reyhanfikri <yamato.reyhan123@gmail.com>
2018-05-17 15:16:06 +0700

    Edit routes, autoload, config

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['part'] = 'part';
 $route['login'] = 'login';
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
