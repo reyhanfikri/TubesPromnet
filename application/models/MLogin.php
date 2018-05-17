f9346d4d6568fc3f37443c47231298f0b0f34c60
reyhanfikri <yamato.reyhan123@gmail.com>
2018-05-17 15:15:47 +0700

    Melengkapi login

---
+++ application/models/MLogin.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MLogin extends CI_Model
+{
+	public function __construct() {
+
+		parent::__construct();
+	}
+
+	public function login($username, $password) {
+
+		$this->db->select('*');
+		$this->db->where('username', $username);
+		$this->db->where('password', $password);
+
+		$user = $this->db->get('t_user');
+
+		if ($user->num_rows() > 0){
+
+			return $user;
+
+		} else {
+
+			return FALSE;
+
+		}
+
+	}
+}
